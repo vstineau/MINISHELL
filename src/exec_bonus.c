d93300c9b69666063956de3e0097ddb2c1041a2d
aroualid <aroualid@student.42.fr>
2024-07-25 18:54:11 +0200

    FIX

--- src/exec_bonus.c
+++ src/exec_bonus.c
@@ -38,37 +38,45 @@ int	init_sigquit(t_minishell *info)
 	return (1);
 }
 
-void	exec(t_minishell *info, t_cmd *c)
+void	check_dobble_pipe(t_cmd *test, int pipout)
 {
-	int	i;
-	int	pipout;
-	t_cmd *test;
-	
-	test = c;
-	i = 0;
-	pipout = 42;
-	if (init_sigquit(info) == 0)
-		exit_free_perror(c, ENV, info,"");
 	while (test)
 	{
 		if (test->pipe == PIPE)
 			test->next->previous_pipe = 1;
-		if (test->previous_pipe == 1 && test->pipe == 0 && !test->cmd && !test->infile && !test->outfile)
+		if (test->previous_pipe == 1 && test->pipe == 0
+			&& !test->cmd && !test->infile && !test->outfile)
 		{
 			close (pipout);
-			return(ft_putstr_fd("syntax error near unexpected token `|'\n", 2));
+			return (ft_putstr_fd("syntax error near unexpected token `|'\n", 2));
 		}
 		test = test->next;
 	}
-	while (c)
+}
+
+void	exec(t_minishell *info, t_cmd *c)
+{
+	int		i;
+	int		pipout;
+	t_cmd	*test;
+
+	test = c;
+	i = 0;
+	pipout = 42;
+	if (init_sigquit(info) == 0)
+		exit_free_perror(c, ENV, info, "");
+	check_dobble_pipe(test, pipout);
 	{
-		if (c->cmd)
-			pipout = exec_midle(info, pipout, c);
-		if (c->pipe == PIPE)
-			c->next->previous_pipe = 1;
-		c = c->next;
+		while (c)
+		{
+			if (c->cmd)
+				pipout = exec_midle(info, pipout, c);
+			if (c->pipe == PIPE)
+				c->next->previous_pipe = 1;
+			c = c->next;
+		}
+		while (wait(NULL) > 0)
+			;
+		close (pipout);
 	}
-	while (wait(NULL) > 0)
-		;
-	close (pipout);
 }
