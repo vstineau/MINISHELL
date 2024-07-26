fea93691ccf237f3c1f8746e9f467f224529fa29
aroualid <aroualid@student.42.fr>
2024-07-26 10:16:55 +0200

    norm

--- includes/minishell.h
+++ includes/minishell.h
@@ -151,4 +151,5 @@ void		our_env(char **env, int fd);
 void		our_exit(t_cmd *c, t_minishell *info);
 char		**our_export(char **av, char **env, int fd);
 char		**unset(char **av, char **env);
+
 #endif
