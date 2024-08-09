943ffbad18356f30563c44ef9e37b7a51d2dc38e
aroualid <aroualid@student.42.fr>
2024-08-09 15:51:03 +0200

    fixcon

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: vstineau <vstineau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 14:54:16 by vstineau          #+#    #+#             */
-/*   Updated: 2024/08/09 14:57:48 by vstineau         ###   ########.fr       */
+/*   Updated: 2024/08/09 15:48:51 by aroualid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,42 +49,27 @@ __int128	ato__i128(const char *str)
 	return (resultat * signe);
 }
 
-//static int	free_cmd2(t_cmd *cmd, bool env, t_minishell *info)
-//{
-//	t_cmd	*temp;
-//
-//	if (env)
-//		free_split(info->env);
-//	while (cmd)
-//	{
-//		temp = cmd;
-//		if (cmd->cmd)
-//			free(cmd->cmd);
-//		if (cmd->infile && cmd->redirect == NO_HEREDOC)
-//			free(cmd->infile);
-//		if (cmd->outfile)
-//			free(cmd->outfile);
-//		free_split(cmd->arg);
-//		cmd = cmd->next;
-//		free(temp);
-//	}
-//	unlink("heredoc");
-//	cmd = NULL;
-//	return (0);
-//}
+void	to_many_arg(t_cmd *c)
+{
+	ft_putstr_fd("exit\n", 2);
+	ft_putstr_fd("exit: too many arguments\n", 2);
+	c->i->code_error = 1;
+	return ;
+}
 
 void	our_exit(t_cmd *c, t_cmd *c_first, int pip[2], int fd)
 {
-	int			i;
-	__int128	atoi_crack;
-	int			value;
+	int				i;
+	__int128		atoi_crack;
+	int				value;
 
 	value = 0;
 	i = 0;
 	if (c->arg[0] == NULL)
 		free_and_close(fd, pip, c_first, 0);
+	if (c->arg[1] != NULL)
+		return (to_many_arg(c));
 	atoi_crack = ato__i128(c->arg[0]);
-	printf("\n");
 	if (c->arg[0][0] == '-')
 		i = 1;
 	while (i < ft_strlen(c->arg[0]))
