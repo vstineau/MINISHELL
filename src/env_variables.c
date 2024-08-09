b34c99f80e8e392af0e370de2f9be7d321927202
Vincent Stineau <vstineau@bess-f2r1s10.clusters.42paris.fr>
2024-08-09 13:49:57 +0200

    fix

--- src/env_variables.c
+++ src/env_variables.c
@@ -22,7 +22,7 @@ static void	fill_key(char key[4096], char *s, int *j, t_iterator *a)
 		key[*j - a->i - 1] = s[*j];
 		*j += 1;
 	}
-	if (s[*j] && !check_char(s[*j], "$\" \t/"))
+	if (s[*j] && !check_char(s[*j], "$\"= \t/"))
 		key[*j - a->i - 2] = 4;
 	if (s[*j] && s[*j] == '$' && (!s[*j + 1] || check_char(s[*j + 1], "\" \t")))
 		a->dols_end = true;
