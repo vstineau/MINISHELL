8c2b38740e94b0da257f05f1b42e42d73fbc81dc
Vincent Stineau <vstineau@made-f0Ar1s1.clusters.42paris.fr>
2024-08-13 15:55:52 +0200

    fix \n

--- src/cmd.c
+++ src/cmd.c
@@ -6,7 +6,7 @@
 /*   By: vstineau <vstineau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 14:53:49 by vstineau          #+#    #+#             */
-/*   Updated: 2024/08/13 15:51:22 by aroualid         ###   ########.fr       */
+/*   Updated: 2024/08/13 15:55:19 by vstineau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ static int	if_cmd(char *s, t_cmd *c, int *i_arg, t_minishell *info)
 			BG_RED"memory allocation failed during parsing\n"RESET);
 	i = 0;
 	j = 0;
-	while (!c->error && s[i] && s[i] != ' ' && s[i] != '\t' && s[i] != '|' && s[i] != '\n')
+	while (!c->error && s[i] && !check_char(s[i], " \t\n|"))
 	{
 		if (s[i] == '\'' || s[i] == '"')
 		{
